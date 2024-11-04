3539f57f43a7ec9440e222961c7600733568f1e5
upiidesu <luthfifauzan857@gmail.com>
2024-11-04 15:51:12 +0700

    membuat loading animation

--- login/script.js
+++ login/script.js
@@ -20,3 +20,29 @@ daftar.addEventListener('click', () => {
 masuk.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const daftarBtn = document.querySelector('.btn.daftar');
+const masukBtn = document.querySelector('.btn.masuk');
+const loaderDaftar = daftarBtn.querySelector('.loader');
+const loaderMasuk = masukBtn.querySelector('.loader');
+
+// Fungsi untuk menambahkan kelas active dengan delay pada daftar
+daftarBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    loaderDaftar.classList.add('active');
+    
+    setTimeout(() => {
+        loaderDaftar.classList.remove('active');
+    }, 1000);
+});
+
+// Fungsi untuk menampilkan loader dan mengalihkan ke halaman lain pada masuk
+masukBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    loaderMasuk.classList.add('active'); 
+    
+    setTimeout(() => {
+        loaderMasuk.classList.remove('active');
+        window.location.href = "../index.html";
+    }, 1000);
+});
